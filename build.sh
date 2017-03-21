5ce3eb6237bbae90a57fc52281e625fd2bd38d51
Alex Behm <alex.behm@cloudera.com>
2017-03-20 17:48:19 -0700

    Add build type parameter to build.sh.
    Before this patch build.sh always built debug.
    
    Change-Id: I9d69e5602ccdfb57f6c418c246880245208e275a

--- build.sh
+++ build.sh
@@ -4,17 +4,17 @@
 set -euo pipefail
 trap 'echo Error in $0 at line $LINENO: $(cd "'$PWD'" && awk "NR == $LINENO" $0)' ERR
 
-# this script is used to build the impala-lzo bits it assumes that the first
+# This script is used to build the impala-lzo bits. It assumes that the second
 # argument is the path to a checkout folder of the Impala code.
 
-if [ $# -lt 1 ]; then
-  echo "Usage $0 impala_dir [toolchain]"
+if [ $# -lt 2 ]; then
+  echo "Usage $0 build_type impala_dir [toolchain_dir]"
   exit 1
 fi
 
-export IMPALA_HOME=$1
+export IMPALA_HOME=$2
 export IMPALA_LZO_HOME="$( cd "$( dirname "${BASH_SOURCE[0]}" )/" && pwd )"
-export IMPALA_TOOLCHAIN=${2:-${IMPALA_HOME}/toolchain}
+export IMPALA_TOOLCHAIN=${3:-${IMPALA_HOME}/toolchain}
 
 cd "$IMPALA_LZO_HOME"
 
@@ -29,7 +29,7 @@ popd
 rm -rf CMakeCache.txt CMakeFiles
 
 MAKE_CMD=${MAKE_CMD:-make}
-CMAKE_FLAGS=
+CMAKE_FLAGS=" -DCMAKE_BUILD_TYPE=$1"
 if [[ "$MAKE_CMD" = "ninja" ]]; then
   CMAKE_FLAGS+=" -GNinja"
 fi
